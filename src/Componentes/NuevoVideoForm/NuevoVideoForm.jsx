9c55cb3e605c588372e390b1b90a46e28216934c
2gutierrez <159669831+2gutierrez@users.noreply.github.com>
2024-07-12 21:49:14 -0700

    Add files via upload

--- src/Componentes/NuevoVideoForm/NuevoVideoForm.jsx
+++ src/Componentes/NuevoVideoForm/NuevoVideoForm.jsx
@@ -37,74 +37,82 @@ const NuevoVideoForm = ({ onSubmit, onClose }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="nuevo-video-form">
+    <div className="nuevo-video-form">
       <h1 className="titulo-modal">Nuevo Video</h1>
-        <p>Complete el formulario para crear una nueva tarjeta de video</p>
-      <div className="form-group">
-        <label htmlFor="titulo">Título</label>
-        <input
-          type="text"
-          id="titulo"
-          name="titulo"
-          value={formValues.titulo}
-          onChange={handleChange}
-          required
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="categoria">Categoría</label>
-        <select
-          id="categoria"
-          name="categoria"
-          value={formValues.categoria}
-          onChange={handleChange}
-          required
-        >
-          <option value="">Seleccionar</option>
-          <option value="frontend">Frontend</option>
-          <option value="backend">Backend</option>
-          <option value="innovacion">Innovación y Gestión</option>
-        </select>
-      </div>
-      <div className="form-group">
-        <label htmlFor="imagen">Imagen</label>
-        <input
-          type="text"
-          id="imagen"
-          name="imagen"
-          value={formValues.imagen}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="video">Video</label>
-        <input
-          type="text"
-          id="video"
-          name="video"
-          value={formValues.video}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="descripcion">Descripción</label>
-        <textarea
-          id="descripcion"
-          name="descripcion"
-          value={formValues.descripcion}
-          onChange={handleChange}
-          required
-        ></textarea>
-      </div>
-      <div className="form-buttons">
-        <button type="submit">
-          <img src={BotonGuardar} alt="Guardar" />
-        </button>
-        <button type="button" className="limpiar" onClick={handleReset}>
-          <img src={BotonLimpiar} alt="Limpiar" />
-        </button>
-      </div>
-    </form>
+      <p>Complete el formulario para crear una nueva tarjeta de video</p>
+      <form onSubmit={handleSubmit}>
+        <div className="form-row">
+          <div className="form-group">
+            <label htmlFor="titulo">Título</label>
+            <input
+              type="text"
+              id="titulo"
+              name="titulo"
+              value={formValues.titulo}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="categoria">Categoría</label>
+            <select
+              id="categoria"
+              name="categoria"
+              value={formValues.categoria}
+              onChange={handleChange}
+              required
+            >
+              <option value="">Seleccionar</option>
+              <option value="frontend">Frontend</option>
+              <option value="backend">Backend</option>
+              <option value="innovacion">Innovación y Gestión</option>
+            </select>
+          </div>
+        </div>
+        <div className="form-row">
+          <div className="form-group">
+            <label htmlFor="imagen">Imagen</label>
+            <input
+              type="text"
+              id="imagen"
+              name="imagen"
+              value={formValues.imagen}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="video">Video</label>
+            <input
+              type="text"
+              id="video"
+              name="video"
+              value={formValues.video}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        <div className="form-row descripcion-container">
+          <div className="form-group">
+            <label htmlFor="descripcion">Descripción</label>
+            <textarea
+              id="descripcion"
+              name="descripcion"
+              value={formValues.descripcion}
+              onChange={handleChange}
+              required
+            ></textarea>
+          </div>
+        </div>
+        <div className="form-buttons">
+          <button type="submit">
+            <img src={BotonGuardar} alt="Guardar" />
+          </button>
+          <button type="button" className="limpiar" onClick={handleReset}>
+            <img src={BotonLimpiar} alt="Limpiar" />
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
